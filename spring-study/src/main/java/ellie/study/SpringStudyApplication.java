b477de647f97447700dd07bc6a079ebe4f12f37b
RIANAEH <ririhan217@gmail.com>
2024-07-13 00:31:49 +0900

    [develop] SpringBoot 프로젝트 생성

---
+++ spring-study/src/main/java/ellie/study/SpringStudyApplication.java
@@ -0,0 +1,14 @@
+package ellie.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ spring-study/src/test/java/ellie/study/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package ellie.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
